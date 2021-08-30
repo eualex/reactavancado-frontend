721035ecdfe0a27c06f14acf0ba63d1751bc8ce8
eualex <allex.araujo088@gmail.com>
2021-08-30 11:02:40 -0300

    REFACTOR: abstracting function that fetch data to landing page

---
+++ src/graphql/fetchs/fetchLandingPage.ts
@@ -0,0 +1,78 @@
+import { client } from 'graphql/client'
+import { GET_LANDING_PAGE } from 'graphql/queries/getLandingPage'
+import { formatPrice } from 'utils/formatPrice'
+import { getImageUrl } from 'utils/getImageUrl'
+import { LandingPageProps } from 'types/api'
+
+export async function fecthLandingPageData() {
+  const { landingPage } = await client.request<{
+    landingPage: LandingPageProps
+  }>(GET_LANDING_PAGE)
+
+  const logo = {
+    ...landingPage.logo,
+    url: getImageUrl(landingPage.logo.url)
+  }
+
+  const header = {
+    ...landingPage.header,
+    image: {
+      ...landingPage.header.image,
+      url: getImageUrl(landingPage.header.image.url)
+    }
+  }
+
+  const sectionAboutProject = {
+    ...landingPage.sectionAboutProject,
+    image: {
+      ...landingPage.sectionAboutProject.image,
+      url: getImageUrl(landingPage.sectionAboutProject.image.url)
+    }
+  }
+
+  const sectionTech = {
+    title: landingPage.sectionTech.title,
+    techIcons: landingPage.sectionTech.techIcons.map((i) => ({
+      title: i.title,
+      icon: {
+        name: i.icon.name,
+        url: getImageUrl(i.icon.url)
+      }
+    }))
+  }
+
+  const pricingBox = {
+    ...landingPage.pricingBox,
+    totalPrice: formatPrice(landingPage.pricingBox.totalPrice),
+    priceInstallment: formatPrice(landingPage.pricingBox.priceInstallment)
+  }
+
+  const sectionAboutUs = {
+    title: landingPage.sectionAboutUs.title,
+    authors: landingPage.sectionAboutUs.authors.map((author) => ({
+      ...author,
+      photo: {
+        alternativeText: author.photo.alternativeText,
+        url: getImageUrl(author.photo.url)
+      },
+      socialLinks: author.socialLinks.map((socialLink) => ({
+        title: socialLink.title,
+        url: getImageUrl(socialLink.url)
+      }))
+    }))
+  }
+
+  return {
+    data: {
+      logo,
+      header,
+      sectionAboutProject,
+      sectionTech,
+      sectionConcepts: landingPage.sectionConcepts,
+      sectionModules: landingPage.sectionModules,
+      sectionAgenda: landingPage.sectionAgenda,
+      pricingBox,
+      sectionAboutUs
+    }
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { GetStaticProps } from 'next'
-import { client } from 'graphql/client'
-import { GET_LANDING_PAGE } from 'graphql/queries/getLandingPage'
 import { LandingPageProps } from 'types/api'
-import { getImageUrl } from 'utils/getImageUrl'
-import { formatPrice } from 'utils/formatPrice'
+import { fecthLandingPageData } from 'graphql/fetchs/fetchLandingPage'
 
 import SectionHero from 'components/SectionHero'
 import SectionAboutProject from 'components/SectionAboutProject'
@@ -47,75 +44,11 @@ const Index = ({
 )
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { landingPage } = await client.request<{
-    landingPage: LandingPageProps
-  }>(GET_LANDING_PAGE)
-
-  const logo = {
-    ...landingPage.logo,
-    url: getImageUrl(landingPage.logo.url)
-  }
-
-  const header = {
-    ...landingPage.header,
-    image: {
-      ...landingPage.header.image,
-      url: getImageUrl(landingPage.header.image.url)
-    }
-  }
-
-  const sectionAboutProject = {
-    ...landingPage.sectionAboutProject,
-    image: {
-      ...landingPage.sectionAboutProject.image,
-      url: getImageUrl(landingPage.sectionAboutProject.image.url)
-    }
-  }
-
-  const sectionTech = {
-    title: landingPage.sectionTech.title,
-    techIcons: landingPage.sectionTech.techIcons.map((i) => ({
-      title: i.title,
-      icon: {
-        name: i.icon.name,
-        url: getImageUrl(i.icon.url)
-      }
-    }))
-  }
-
-  const pricingBox = {
-    ...landingPage.pricingBox,
-    totalPrice: formatPrice(landingPage.pricingBox.totalPrice),
-    priceInstallment: formatPrice(landingPage.pricingBox.priceInstallment)
-  }
-
-  const sectionAboutUs = {
-    title: landingPage.sectionAboutUs.title,
-    authors: landingPage.sectionAboutUs.authors.map((author) => ({
-      ...author,
-      photo: {
-        alternativeText: author.photo.alternativeText,
-        url: getImageUrl(author.photo.url)
-      },
-      socialLinks: author.socialLinks.map((socialLink) => ({
-        title: socialLink.title,
-        url: getImageUrl(socialLink.url)
-      }))
-    }))
-  }
+  const { data } = await fecthLandingPageData()
 
   return {
     props: {
-      ...landingPage,
-      logo,
-      header,
-      sectionAboutProject,
-      sectionTech,
-      sectionConcepts: landingPage.sectionConcepts,
-      sectionModules: landingPage.sectionModules,
-      sectionAgenda: landingPage.sectionAgenda,
-      pricingBox,
-      sectionAboutUs
+      ...data
     }
   }
 }
